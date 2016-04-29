5299bd150566173d9ca65f1896b4ea078c51b781
龚彦铭 <Gongyanming@camera360.com>
2016-04-29 15:18:12 +0800

    新建工程

---
+++ Puzzle-Tool/Puzzle-Tool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Puzzle-Tool
+//
+//  Created by 龚彦铭 on 16/4/29.
+//  Copyright © 2016年 龚彦铭. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Puzzle-Tool/Puzzle-Tool/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Puzzle-Tool
+//
+//  Created by 龚彦铭 on 16/4/29.
+//  Copyright © 2016年 龚彦铭. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
